4c3c8c209e17346cd1f6622d8d041480fa912a8a
jayanta <jayanta.garu@arithaconsulting.com>
2023-11-08 14:56:14 +0530

    as

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,72 @@
-'use client';
+'use client'
+import { useState } from 'react';
+import { Group, Code } from '@mantine/core';
+import {
+  IconBellRinging,
+  IconFingerprint,
+  IconKey,
+  IconSettings,
+  Icon2fa,
+  IconDatabaseImport,
+  IconReceipt2,
+  IconSwitchHorizontal,
+  IconLogout,
+} from '@tabler/icons-react';
+import { MantineLogo } from '@mantine/ds';
+import classes from './ColorSchemeToggle.module.css';
 
-import { Button, Group, useMantineColorScheme } from '@mantine/core';
+const data = [
+  { link: '', label: 'Notifications', icon: IconBellRinging },
+  { link: '', label: 'Billing', icon: IconReceipt2 },
+  { link: '', label: 'Security', icon: IconFingerprint },
+  // { link: '', label: 'SSH Keys', icon: IconKey },
+  // { link: '', label: 'Databases', icon: IconDatabaseImport },
+  // { link: '', label: 'Authentication', icon: Icon2fa },
+  // { link: '', label: 'Other Settings', icon: IconSettings },
+];
 
 export function ColorSchemeToggle() {
-  const { setColorScheme } = useMantineColorScheme();
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
-    </Group>
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        <Group className={classes.header} justify="space-between">
+          <MantineLogo size={28} inverted style={{ color: 'white' }} />
+          <Code fw={700} className={classes.version}>
+            v3.1.2
+          </Code>
+        </Group>
+        {links}
+      </div>
+
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
   );
 }
